b112bb5b132c6eae765ecea2393275f3ede30ad0
Alex Tkachev <tkachev.alex@gmail.com>
2015-12-15 12:31:06 +0200

    removed hash-ext since its present in active support

--- Gemfile
+++ Gemfile
@@ -7,8 +7,8 @@ source "http://rubygems.org"
 # Include everything needed to run rake, tests, features, etc.
 group :development do
   gem "rdoc"
-  gem "rspec", "~> 2.3.0"
-  gem "bundler", "~> 1.0.0"
-  gem "jeweler", "~> 1.6.4"
+  gem "rspec"
+  gem "bundler"
+  gem "jeweler", "~> 1.8.8"
   gem "rcov", ">= 0"
 end
--- lib/rego-ruby-ext.rb
+++ lib/rego-ruby-ext.rb
@@ -1,4 +1,4 @@
-['nil-ext', 'boolean-ext', 'date-ext', 'time-ext', 'enumerable-ext', 'hash-ext', 'numeric-ext', 'symbol-ext', 'string-interpolation', 'string-ext'].each do |file_name|
+['nil-ext', 'boolean-ext', 'date-ext', 'time-ext', 'enumerable-ext', 'numeric-ext', 'symbol-ext', 'string-interpolation', 'string-ext'].each do |file_name|
   require File.join(File.dirname(__FILE__), file_name)
 end
 
